6d829842b1aa228b0fa4d14e50b45f8f4ff2e481
Evan Liu <qingyan123@gmail.com>
2012-05-23 02:21:42 +0800

    AroundMeActivity extends MapActivity now

--- AroundMe/src/info/liuqy/adc/aroundme/AroundMeActivity.java
+++ AroundMe/src/info/liuqy/adc/aroundme/AroundMeActivity.java
@@ -1,13 +1,37 @@
 package info.liuqy.adc.aroundme;
 
-import android.app.Activity;
+import java.util.List;
+
 import android.os.Bundle;
 
-public class AroundMeActivity extends Activity {
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+
+public class AroundMeActivity extends MapActivity {
+
+    MapView mapView;
+    MapController mapCtrl;
+    List<Overlay> mapOverlays;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+
+        mapOverlays = mapView.getOverlays();
+        
+        mapCtrl = mapView.getController();
     }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
