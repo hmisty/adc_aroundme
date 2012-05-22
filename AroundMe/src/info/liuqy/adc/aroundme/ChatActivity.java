da87916dca84945468c18e7749d775c5997dc147
Evan Liu <qingyan123@gmail.com>
2012-05-23 03:35:10 +0800

    let's implement send chat to server first. implement onTap for StarOverlay.

--- AroundMe/src/info/liuqy/adc/aroundme/ChatActivity.java
+++ AroundMe/src/info/liuqy/adc/aroundme/ChatActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 
 public class ChatActivity extends Activity {
+	public static final String EXTRA_ID = "id";
+	public static final String EXTRA_ATTRIBUTES = "attributes";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
