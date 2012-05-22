480f7de3c1732bb739ede65e84a018a636aa309d
Evan Liu <qingyan123@gmail.com>
2012-05-23 03:09:00 +0800

    the chat layout xml and chat activity

---
+++ AroundMe/src/info/liuqy/adc/aroundme/ChatActivity.java
@@ -0,0 +1,14 @@
+package info.liuqy.adc.aroundme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChatActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.chat);
+	}
+
+}
